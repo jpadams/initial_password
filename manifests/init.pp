ed3a1e9676e1bb6dcb6e006851f856cbe942d120
jpadams <jeremy@puppetlabs.com>
2015-05-05 22:06:33 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,3 @@
-class initial_password {
-
-  initial_password::user { 'test1':
-    password_hash => '$1$v4K9E8Wj$gZIHJ5JtQL5ZGZXeqSSsd0',
-  }
-  initial_password::user { 'test2':
-    password_hash => '$1$v4K9E8Wj$gZIHJ5JtQL5ZGZXeqSSsd0',
-  }
-  initial_password::user { 'test3':
-    password_hash => '$1$v4K9E8Wj$gZIHJ5JtQL5ZGZXeqSSsd0',
-  }
-  initial_password::user { 'test4':
-    password_hash => '$1$v4K9E8Wj$gZIHJ5JtQL5ZGZXeqSSsd0',
-  }
-
-}
+# all of the action is in the defined type
+# see tests/init.pp for example usage
+class initial_password {}
